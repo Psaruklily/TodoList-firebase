e2f73268aa5370e66ec56daf4941e5f43c504813
Psaruklily <psarukli96@gmail.com>
2021-02-01 21:19:15 +0300

    Added styles. Finished

--- script.js
+++ script.js
@@ -11,48 +11,14 @@ let firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 //firebase.analytics();
 
-let buttonSave = document.querySelector('#save');
-let buttonUpdate = document.querySelector('#update')
-let taskInput = document.querySelector('#content-task');
-let output = document.querySelector('.output ul');
+const messageForm = document.querySelector('form');
+let buttonSave = document.querySelector('.save-btn');
+let buttonUpdate = document.querySelector('.update-btn')
+let buttonCleanUp = document.querySelector('.сlean-up');
+let taskInput = document.querySelector('[name = "message"]');
+let output = document.querySelector('.list ul');
 
-buttonSave.addEventListener('click', () => {
-    firebase.database().ref('tasks').push().set({
-        task: taskInput.value,
-        date: Date.now()
-    })
-
-})
-
-function outputData(currentData, id) {
-    let li = document.createElement('li');
-    let p = document.createElement('p');
-    p.innerHTML = currentData.task;
-    p.style.display = 'inline'
-    li.appendChild(p)
-    output.appendChild(li)
-
-    let btnUpdate = document.createElement('button');
-    btnUpdate.innerHTML = 'Update';
-    li.appendChild(btnUpdate);
-
-    let btnDelete = document.createElement('button');
-    btnDelete.innerHTML = 'Delete';
-    li.appendChild(btnDelete);
-
-    btnUpdate.addEventListener('click', (event) => {
-        taskInput.value = currentData.task;
-        taskInput.value = currentData.task;
-        let activeLi = event.target.parentElement
-        updateData(id, activeLi, currentData)
-    })
-
-    btnDelete.addEventListener('click', () => {
-        firebase.database().ref('tasks').child(id).remove();
-        li.parentNode.removeChild(li);
-    })
-}
-
+//Отримую дані з database
 function getData(callback) {
     firebase.database().ref('tasks').on('child_added', snap => {
         let val = snap.val();
@@ -60,11 +26,94 @@ function getData(callback) {
         callback(val, key)
     })
 }
-
 getData(outputData);
+
+//Побудова конкретної таски
+function outputData(currentData, id) {
+    let li = document.createElement('li');
+    let mainDiv = document.createElement('div');
+    let task = document.createElement('div');
+    let divForButn = document.createElement('div');
+    let p = document.createElement('p');
+    p.textContent = currentData.task;
+    p.classList.add('task-text');
+    mainDiv.classList.add('main-div-li');
+
+    let checkbox = document.createElement('input')
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('checkbox')
+    checkbox.onclick = function() {
+        if (checkbox.checked == true) {
+            p.style.textDecoration = 'line-through'
+        } else {
+            p.style.textDecoration = 'none'
+        }
+    }
+
+    task.classList.add('task');
+    task.appendChild(checkbox);
+    task.appendChild(p);
+    mainDiv.appendChild(task);
+
+    let update = document.createElement('button');
+    update.innerHTML = '<i class="fas fa-pencil-alt"></i>';
+    update.style.color = '#fff';
+    update.classList.add('update-small');
+
+    let cancel = document.createElement('button');
+    cancel.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    cancel.style.color = '#fff';
+    cancel.classList.add('delete-small');
+
+    divForButn.appendChild(update)
+    divForButn.appendChild(cancel)
+
+    mainDiv.appendChild(divForButn)
+
+    li.appendChild(mainDiv)
+    output.appendChild(li);
+    let hr = document.createElement('hr')
+    li.appendChild(hr);
+
+    update.addEventListener('click', () => {
+        buttonSave.classList.add('display-none');
+        buttonUpdate.classList.remove('display-none');
+        taskInput.value = currentData.task;
+        updateData(id, li);
+    })
+
+
+    cancel.addEventListener('click', () => {
+        firebase.database().ref('tasks').child(id).remove();
+        li.parentNode.removeChild(li);
+    })
+}
+
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    firebase.database().ref('tasks').push().set({
+        task: taskInput.value,
+        date: Date.now()
+    })
+    taskInput.value = '';
+})
+
+
+// MY --------------------------------------------------------------------------------------------------------
+buttonSave.addEventListener('click', () => {
+    firebase.database().ref('tasks').push().set({
+        task: taskInput.value,
+        date: Date.now()
+    })
+    taskInput.value = '';
+})
+
 
 function updateData(id, li) {
     buttonUpdate.addEventListener('click', () => {
+        buttonUpdate.classList.add('display-none');
+        buttonSave.classList.remove('display-none')
         firebase.database().ref('tasks').child(id).update({
             task: taskInput.value,
             date: Date.now(),
@@ -79,3 +128,5 @@ function updateData(id, li) {
         firebase.database().ref('tasks').off('child_changed')
     })
 }
+
+// MY END--------------------------------------------------------------------------------------------------
